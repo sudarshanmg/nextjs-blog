1ad53667b7f2853578eaa81a4a250e0b92b3ec62
sudarshan <sudarshanmallibhat@gmail.com>
2023-02-03 14:08:19 +0530

    second commit

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,8 @@
 import Head from 'next/head';
-import Layout, { siteTitle } from '../components/layout';
+import Layout, { siteTitle } from '../components/Layout';
 import utilStyles from '../styles/utils.module.css';
 import Link from 'next/link';
-import Date from '../components/date';
+import Date from '../components/Date';
 import { getSortedPostsData } from '../lib/posts';
 
 export async function getStaticProps() {
--- pages/posts/[id].jsx
+++ pages/posts/[id].jsx
@@ -1,6 +1,6 @@
-import Layout from '../../components/layout';
+import Layout from '../../components/Layout';
 import Head from 'next/head';
-import Date from '../../components/date';
+import Date from '../../components/Date';
 import { getAllPostIds, getPostData } from '../../lib/posts';
 import utilStyles from '../../styles/utils.module.css';
 
